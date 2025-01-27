70e6a18f2a246597593ce00a2a78c6ab3a60d680
nilaykansagara <nilaykansagara@gmail.com>
2025-01-27 12:25:41 +0530

    feat: achieve S of SOLID in creation of objectives

--- backend/express-server/app.js
+++ backend/express-server/app.js
@@ -5,26 +5,40 @@ const app = express();
 
 app.use(express.json());
 
-const port = 3005;
+const PORT = 3005;
 
-const mockObjective = {
-    id:  faker.string.uuid(),
-    title: faker.company.buzzPhrase(),
-    keyResults: [
-        {
-            title:faker.company.catchPhrase(),
-            initialValue: faker.number.int({ min: 0, max: 10 }),
-            currentValue: faker.number.int({ min: 10, max: 50 }),
-            targetValue: faker.number.int({ min: 50, max: 100 }),
+const MOCK_OBJECTIVES = []
+
+function createKeyResults() {
+    const KEY_RESULTS = []
+    for (let j = 0; j < 4; j++) {
+        const KEY_RESULT = {
+            title: faker.company.catchPhrase(),
+            initialValue: faker.number.int({min: 0, max: 10}),
+            currentValue: faker.number.int({min: 10, max: 50}),
+            targetValue: faker.number.int({min: 50, max: 100}),
             metrics: faker.company.buzzAdjective()
         }
-    ]
+        KEY_RESULTS.push(KEY_RESULT);
+    }
+    return KEY_RESULTS;
 }
 
-app.get('/', (req, res) => {
-    res.status(200).json(mockObjective);
+for (let i = 0; i < 5; i++) {
+
+    const MOCK_OBJECTIVE = {
+        id: faker.string.uuid(),
+        title: faker.company.buzzPhrase(),
+        keyResults: createKeyResults()
+    };
+    MOCK_OBJECTIVES.push(MOCK_OBJECTIVE);
+}
+
+
+app.get('/objectives', (req, res) => {
+    res.status(200).json(MOCK_OBJECTIVES);
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on: ${port}...`);
+app.listen(PORT, () => {
+    console.log(`Server is running on: ${PORT}...`);
 });
