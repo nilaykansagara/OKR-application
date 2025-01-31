7d9a1df4651fbddb0c6cd370ef8a7162f490ab70
nilaykansagara <nilaykansagara@gmail.com>
2025-01-31 13:00:17 +0530

    test: e2e for hello world module with supertest vs axios time requirement

--- backend/server-nest/test/hello-world.e2e-spec.ts
+++ backend/server-nest/test/hello-world.e2e-spec.ts
@@ -1,27 +1,42 @@
 import {INestApplication} from "@nestjs/common";
 import {Test} from "@nestjs/testing";
 import {AppModule} from "../src/app.module";
-import axios from "axios";
+import * as request from 'supertest';
+import axios from 'axios';
 
 describe('hello-world(Integration)', () => {
 
     let app: INestApplication;
+    let app2: INestApplication;
     const PORT = 3000
     beforeEach(async () => {
         const module = await Test.createTestingModule({
             imports: [AppModule]
         }).compile();
 
+        const module2 = await Test.createTestingModule({
+            imports: [AppModule]
+        }).compile();
+
         app = module.createNestApplication();
-        await app.listen(PORT);
+        app2 = module2.createNestApplication();
+        await app.init();
+        await app2.listen(PORT);
     });
 
     afterEach(async () => {
-        await app.close();
+        await app2.close();
     });
 
-    it('@Get should return Hello world', async () => {
+    it('AXIOS: @Get should return Hello world', async () => {
         const response = await axios.get(`http://localhost:${PORT}/hello-world/`);
         expect(response.data).toBe('Hello World!');
     })
+
+    it.only('Supertest: @Get should return Hello world', async () => {
+        const response = await request(app.getHttpServer()).get('/hello-world/');
+        expect(response.text).toBe('Hello World!');
+    })
+
+
 })
