3daea82610dce4d62cb5c232368fb711b5fb634a
nilaykansagara <nilaykansagara@gmail.com>
2025-02-02 23:00:39 +0530

    feat: connect backend with frontend to show stored data

--- frontend/src/OKR-store/OKR-Data.ts
+++ frontend/src/OKR-store/OKR-Data.ts
@@ -1,15 +1,35 @@
-import {ObjectiveType, ObjectiveTypeWithId} from "../Types/OKRTypes.ts";
+import {KeyResultType, ObjectiveType, ObjectiveTypeWithId} from "../Types/OKRTypes.ts";
 
-const jsonAPI = "http://localhost:3005/objectives";
+const jsonAPI = "http://localhost:3000/objectives";
+const objectivesAPI = "http://localhost:5040/objectives";
+const keyResultsAPI = "http://localhost:5040/key-results";
 
 
 async function getOKRData(): Promise<ObjectiveTypeWithId[]> {
-    const response = await fetch(jsonAPI, {method: "GET"});
-    return await response.json();
 
-    // const objectives = await fetch(objectivesAPI, {method: "GET"});
-    // const keyResults = await fetch(keyResultsAPI, {method: "GET"});
-    // const objectivesWithId[]:ObjectiveTypeWithId[];
+    const objectives = await fetch(objectivesAPI, {method: "GET"});
+    const keyResults = await fetch(keyResultsAPI, {method: "GET"});
+
+    const objectivesResponse = await objectives.json();
+    const keyResultsResponse = await keyResults.json();
+
+    const objectivesWithId: ObjectiveTypeWithId[] = objectivesResponse.map((objective) => {
+        const relatedKeyResults = keyResultsResponse.filter(keyresult => keyresult.objectiveId === objective.id);
+        const keyResults: KeyResultType[] = relatedKeyResults.map(keyresult => ({
+            title: keyresult.title,
+            initialValue: keyresult.initial_value,
+            currentValue: keyresult.current_value,
+            targetValue: keyresult.target_value,
+            metrics: keyresult.metrics,
+        }));
+        return {
+            id: objective.id,
+            title: objective.title,
+            keyResults
+        };
+    })
+    
+    return objectivesWithId;
 }
 
 async function insertOKRData(objective: ObjectiveType): Promise<void> {
--- frontend/src/Types/OKRTypes.ts
+++ frontend/src/Types/OKRTypes.ts
@@ -1,14 +1,25 @@
 export type KeyResultType = {
-  title: string;
-  initialValue: number;
-  currentValue: number;
-  targetValue: number;
-  metrics: string;
+    title: string;
+    initialValue: number;
+    currentValue: number;
+    targetValue: number;
+    metrics: string;
 };
 export type ObjectiveType = {
-  title: string;
-  keyResults: KeyResultType[ ];
+    title: string;
+    keyResults: KeyResultType[ ];
 };
 export type ObjectiveTypeWithId = ObjectiveType & {
-  id:string,
+    id: string,
+}
+export type ObjectiveDto = {
+    title: string;
+}
+export type KeyResultDto = {
+    title: string;
+    initial_value: number;
+    current_value: number;
+    target_value: number;
+    metrics: string;
+    objectiveId: number;
 }
